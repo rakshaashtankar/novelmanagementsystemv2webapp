20ac706bbf9070d66bf61068356abb0cae7c7f7b
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2025-06-09 00:17:21 +0530

    Update railway.config.js file to resolve railway deploy issues.

--- railway.config.js
+++ railway.config.js
@@ -1,6 +1,13 @@
-// railway.config.js
 module.exports = {
-    root: ".",                     // root of your React app
-    buildCommand: "npm run build", // builds production build
-    startCommand: "serve -s build" // if you are not using Netlify/Vercel-style hosting
-};
+    build: {
+      nixpacksPlan: {
+        phases: {
+          install: {
+            dependsOn: ["setup"],
+            cmds: ["npm install --legacy-peer-deps"]
+          }
+        }
+      }
+    }
+  };
+  
